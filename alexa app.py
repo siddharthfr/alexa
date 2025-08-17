60b04d1fa781201824c1ab95c8f6372db37b037b
Dipu Kumar <dipukumar.csbs2022@nsec.ac.in>
2025-08-17 16:40:35 +0530

    flask-alexa.py

--- alexa app.py
+++ alexa app.py
@@ -1,35 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Satyajit Pattnaik
 
-#Importing Libraries
+"""
+
+from flask import Flask,render_template,redirect,request
+import warnings
+warnings.filterwarnings('ignore')
+
+
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import datetime
-import wikipedia
 import pyjokes
-import requests, json , sys
-#PyAudio
-#PyWhatKit
-#PyJokes
-#Wikipedia
-#OpenweatherApi
+import wikipedia
+import sys
+import requests, json 
 
 listener = sr.Recognizer()
+#engine = pyttsx3.init()
 
-engine = pyttsx3.init()
-voices = engine.getProperty("voices")
-engine.setProperty('voice', voices[1].id)
+import os
+
+app = Flask("__name__")
 
 def engine_talk(text):
+    engine = pyttsx3.init()
+    voices = engine.getProperty("voices")
+    engine.setProperty('voice', voices[1].id)
     engine.say(text)
     engine.runAndWait()
+
     
+def user_commands():
+    try:
+        with sr.Microphone() as source:
+            print("Start Speaking!!")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            command = command.replace('alexa', '')
+            if 'alexa' in command:
+                print(command)
+    except:
+        pass
+    return command
+
 def weather(city):
     # Enter your API key here 
-    api_key = "<YOUR API KEY"
+    api_key = "<YOUR API KEY>"
+    #How to use api_key, see the below code:
+    #api_key = "ABCDE"
     
     # base_url variable to store url 
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
-
+    
     # Give city name 
     city_name = city
     
@@ -59,85 +85,64 @@ def weather(city):
         # store the value corresponding 
         # to the "temp" key of y 
         current_temperature = y["temp"] 
-    
-        # store the value corresponding 
-        # to the "pressure" key of y 
-        #current_pressure = y["pressure"] 
-    
-        # store the value corresponding 
-        # to the "humidity" key of y 
-        #current_humidiy = y["humidity"] 
-    
-        # store the value of "weather" 
-        # key in variable z 
-        #z = x["weather"] 
-    
-        # store the value corresponding  
-        # to the "description" key at  
-        # the 0th index of z 
-        #weather_description = z[0]["description"]
-        return str(current_temperature)
-    
-        # print following values 
-        '''print(" Temperature (in kelvin unit) = " +
-                        str(current_temperature) + 
-            "\n atmospheric pressure (in hPa unit) = " +
-                        str(current_pressure) +
-            "\n humidity (in percentage) = " +
-                        str(current_humidiy) +
-            "\n description = " +
-                        str(weather_description)) 
-    else: 
-        print(" City Not Found ")
-        '''
-
-def user_commands():
-    try:
-        with sr.Microphone() as source:
-            print("Start Speaking!!")
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
-                print(command)
-    except:
-        pass
-    return command
+        temp_in_celcius = current_temperature - 273.15
+        return str(int(temp_in_celcius))
     
     
 def run_alexa():
     command = user_commands()
-    if 'play' in command:
-        song = command.replace('play', '')
-        #print('New Command is' +command)
-        #print('The bot is telling us: Playing' +command)
-        engine_talk('Playing' +song)
+    if 'play a song' in command:
+        song = 'Arijit Singh'
+        engine_talk('Playing some music')
+        print("Playing....")
         pywhatkit.playonyt(song)
+    elif 'play' in command:
+        song = command.replace('play', '')
+        engine_talk('Playing....' + song)
+        print("Playing....")
+        pywhatkit.playonyt(song)     
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%M %p')
-        engine_talk('The current time is' +time)
+        print(time)
+        engine_talk('Current Time is' + time)
+    elif 'joke' in command:
+        get_j = pyjokes.get_joke()
+        print(get_j)
+        engine_talk(get_j)
     elif 'who is' in command:
-        name = command.replace('who is' , '')
-        info =  wikipedia.summary(name, 1)
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
         print(info)
         engine_talk(info)
-    elif 'joke' in command:
-        engine_talk(pyjokes.get_joke())
     elif 'weather' in command:
-        engine_talk('Please tell the name of the city')
-        city = user_commands()
-        #weather_api = weather('Hong Kong')
-        weather_api = weather(city)
-        engine_talk(weather_api + 'degree fahreneit' )
+        #engine_talk('Please tell name of the city')
+        city = 'Hong Kong'
+        #city = 'Mumbai'
+        engine_talk('The temperature in Hong Kong is' + weather(city) + 'degree celcius')
     elif 'stop' in command:
-        sys.exit()
+        engine_talk("Good bye")
     else:
-        engine_talk('I could not hear you properly')
+        engine_talk("I didn't hear you properly")
+        print("I didn't hear you properly")
+
+
+@app.route('/')
+def hello():
+    return render_template("alexa.html")
+
+@app.route("/home")
+def home():
+    return redirect('/')
+
+@app.route('/',methods=['POST', 'GET'])
+def submit():
+    while True:
+        run_alexa()
+    return render_template("alexa.html")
         
-        
-while True:
-    run_alexa()
+
+if __name__ =="__main__":
+    app.run(debug=True)
     
     
     
